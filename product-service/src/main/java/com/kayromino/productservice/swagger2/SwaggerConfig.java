87720dedf1240e3b659d359bfca9e654f065841e
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2020-05-31 21:33:05 +0300

    Refact

--- product-service/src/main/java/com/kayromino/productservice/swagger2/SwaggerConfig.java
+++ product-service/src/main/java/com/kayromino/productservice/swagger2/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,28 +22,11 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build().apiInfo(apiInfo());
     }
 
-    //@Bean
-    //public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//                .apis(RequestHandlerSelectors.basePackage("com.kayromino.orderservice.controller"))
-//                .paths(PathSelectors.ant("/foos/*"))
-//                .build()
-//                .apiInfo(apiInfo())
-//                .useDefaultResponseMessages(false)
-//                .globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500)
-//                                .message("500 message")
-//                                .responseModel(new ModelRef("Error"))
-//                                .build(),
-//                        new ResponseMessageBuilder().code(403)
-//                                .message("Forbidden!!!!!")
-//                                .build()));
-//    }
-
-//    private ApiInfo apiInfo() {
-//        ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service", new Contact("John Doe", "www.example.com", "myeaddress@company.com"), "License of API", "API license URL", Collections.emptyList());
-//        return apiInfo;
-//    }
+    private ApiInfo apiInfo() {
+       ApiInfo apiInfo = new ApiInfo("Product REST API", "Ecommerce product API.", "API v1", "Terms of service", new Contact("Ekren Kamberoglu", "www.kayromino.com", "ekrem@kayromino.com"), "License of API", "API license URL", Collections.emptyList());
+       return apiInfo;
+    }
 }
