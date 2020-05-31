9533839330dcfbd96c79890a675ca5ac04fc9917
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2020-05-31 21:48:25 +0300

    Refact

--- order-service/src/main/java/com/kayromino/orderservice/swagger2/SwaggerConfig.java
+++ order-service/src/main/java/com/kayromino/orderservice/swagger2/SwaggerConfig.java
@@ -1,22 +1,16 @@
 package com.kayromino.orderservice.swagger2;
 
-import static com.google.common.collect.Lists.newArrayList;
-
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -28,28 +22,11 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build().apiInfo(apiInfo());
     }
 
-    //@Bean
-    //public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//                .apis(RequestHandlerSelectors.basePackage("com.kayromino.orderservice.controller"))
-//                .paths(PathSelectors.ant("/foos/*"))
-//                .build()
-//                .apiInfo(apiInfo())
-//                .useDefaultResponseMessages(false)
-//                .globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500)
-//                                .message("500 message")
-//                                .responseModel(new ModelRef("Error"))
-//                                .build(),
-//                        new ResponseMessageBuilder().code(403)
-//                                .message("Forbidden!!!!!")
-//                                .build()));
-//    }
-
-//    private ApiInfo apiInfo() {
-//        ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service", new Contact("John Doe", "www.example.com", "myeaddress@company.com"), "License of API", "API license URL", Collections.emptyList());
-//        return apiInfo;
-//    }
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo("Order REST API", "Ecommerce Order API.", "API v1", "Terms of service", new Contact("Ekren Kamberoglu", "www.kayromino.com", "ekrem@kayromino.com"), "License of API", "API license URL", Collections.emptyList());
+        return apiInfo;
+    }
 }
